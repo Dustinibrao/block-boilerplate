5934faffd365491f1f009f0e549951b15d635a39
Dustin <dustinibrao@gmail.com>
2022-10-24 08:51:03 -1000

    boilerplate

---
+++ src/edit.js
@@ -0,0 +1,11 @@
+import { useBlockProps } from "@wordpress/block-editor";
+import { __ } from "@wordpress/i18n";
+import "./editor.scss";
+
+export default function Edit() {
+	return (
+		<p {...useBlockProps()}>
+			{__("Boilerplate – hello from the editor!", "boilerplate")}
+		</p>
+	);
+}
